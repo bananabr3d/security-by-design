c6f7e900a710e6b94ece3da901255be08a46ff5a
Kxro <90672361+KxroShinigami@users.noreply.github.com>
2023-10-23 12:19:42 +0200

    Updated test_run_tests.py and pytest.ini

--- Customer-Portal/test_run_tests.py
+++ Customer-Portal/test_run_tests.py
@@ -1,7 +1,7 @@
 from app.tests.unit import verify_env
 
 def test_environmentvariables():
-    expected_environment_variables = ["SECRET_KEY", "JWT_SECRET_KEY", "MONGODB_USER", "MONGODB_PW", "MONGODB_CLUSTER", "MONGODB_SUBDOMAIN"]
+    expected_environment_variables = ["SECRET_KEY", "JWT_SECRET_KEY", "MONGODB_USER", "MONGODB_PW", "MONGODB_CLUSTER", "MONGODB_SUBDOMAIN", "JWT_ACCESS_TOKEN_EXPIRATION_MINUTES", "2FA_EXPIRATION_MINUTES"]
 
     assert verify_env.verify_all(expected_environment_variables=expected_environment_variables) == True
 
