805933c1ac292be1db50c6376d1069e1141f69e0
Kxro <90672361+KxroShinigami@users.noreply.github.com>
2023-10-26 08:39:13 +0200

    TLS1.2 -> TLS1.3 in web app

--- Customer-Portal/run.py
+++ Customer-Portal/run.py
@@ -3,6 +3,6 @@ from app import app
 import ssl
 
 if __name__ == "__main__":
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('cert.pem', 'key.pem')
     app.run(host="0.0.0.0", port=5000, ssl_context=context)
