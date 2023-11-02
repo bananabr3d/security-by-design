8f44203ffbe24f3ac6522b628ff0c59c19b01055
Anasailon <flockenterprise@gmail.com>
2023-11-02 09:36:15 +0100

    added load contract data

--- Customer-Portal/app/models/contract.py
+++ Customer-Portal/app/models/contract.py
@@ -6,6 +6,21 @@ from bson.objectid import ObjectId
 def load_contract(db, contract_id):
     return Contract(db.db.contracts.find_one({'_id': ObjectId(contract_id)}))
 
+def load_contract_data(user, db):
+    # Implement the logic to load contract data for the user
+    contract_list = user.get_attribute("contracts")
+    contract_data = []
+
+    for contract_id in contract_list:
+        # Load contract data using the load_contract function
+        contract = load_contract(db, contract_id)
+        contract_data.append({
+            '_id': contract.get_id(),
+            'electricity_meter_id': contract.contract_data.get('electricity_meter_id'),
+            # Add more contract data fields 
+        })
+
+    return contract_data
 class Contract(UserMixin): # Add more details to user
     def __init__(self, contract_data) -> None:
         self.contract_data = contract_data
