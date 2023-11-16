ca0397fc308611288290ab35791b6b239c6a0e7d
Kxro <90672361+KxroShinigami@users.noreply.github.com>
2023-11-16 12:42:10 +0100

    Added nginx container and updated customer-portal flask app.run accordingly. Also added the ssl-bundle.crt for the website cert

--- Customer-Portal/run.py
+++ Customer-Portal/run.py
@@ -1,8 +1,4 @@
 from app import app
 
-import ssl
-
 if __name__ == "__main__":
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain('cert.pem', 'key.pem')
-    app.run(host="0.0.0.0", port=443, ssl_context=context)
+    app.run(host="0.0.0.0", port=5000)
