24d28d0f30820c53fe1e809c38e6f2a49a2ab8a0
Kxro <90672361+KxroShinigami@users.noreply.github.com>
2023-11-19 23:03:45 +0100

    Test!! run github actions

--- Customer-Portal/app/__init__.py
+++ Customer-Portal/app/__init__.py
@@ -180,6 +180,3 @@ bcrypt = Bcrypt(app)
 
 # Security Questions
 security_questions = ["What is your mother's maiden name?", "What is the name of your first pet?", "What is your favorite color?", "What is the name of the city where you were born?", "What is your favorite movie?"]
-
-# === Import routes ===
-from app.routes import routes, contract_routes, auth_routes, error_routes, auth_routes_2fa
