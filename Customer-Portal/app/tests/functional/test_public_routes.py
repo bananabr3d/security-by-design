51415731e6405a3d3ef36b9355fb159a174de9b7
Kxro <90672361+KxroShinigami@users.noreply.github.com>
2023-11-20 12:17:18 +0100

    Added /index and /home

--- Customer-Portal/app/tests/functional/test_public_routes.py
+++ Customer-Portal/app/tests/functional/test_public_routes.py
@@ -7,10 +7,15 @@ class TestPublicRoutes:
         self.client = app.test_client()
 
     # GET Requests
-    def test_get_home_route(self):
+    def test_get_start_route(self):
         response = self.client.get('/')
         assert response.status_code == 200
 
+    def test_get_index_route(self):
+        response = self.client.get('/index')
+        assert response.status_code == 200
+
+    def test_get_home_route(self):
         response = self.client.get('/home')
         assert response.status_code == 200
 
