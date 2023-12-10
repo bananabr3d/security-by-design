afecd92653ee7db09a69309063dd8378672881f5
Anasailon <flockenterprise@gmail.com>
2023-12-10 20:54:47 +0100

    updated test_contract_routes.py

--- Customer-Portal/app/tests/functional/test_contract_routes.py
+++ Customer-Portal/app/tests/functional/test_contract_routes.py
@@ -5,6 +5,7 @@ from app.tests.functional.test_private_routes import add_user_information
 from app.models.user import User
 from app.models.contract import Contract as Contract_class
 from pyotp import TOTP
+from bson.objectid import ObjectId
 
 def add_contract():
     '''
@@ -46,76 +47,80 @@ class TestContractRoutes:
             db.contracts.delete_one({"electricity_meter_id": "pytest"})
 
         # # Check if em with id already exists
-        # if db.electricity_meter.find_one({"_id": ObjectId("111111111111111111111111")}) == None:
-        #     db.electricity_meter.insert_one({"_id": ObjectId("111111111111111111111111")})
+        if db.electricity_meter.find_one({"_id": ObjectId("111111111111111111111111")}) == None:
+            logger.info("Electricity Meteer not existing adding it")
+            db.electricity_meter.insert_one({"_id": ObjectId("111111111111111111111111")})
 
         # # Set em_status to True (free)
-        # db.electricity_meter.update_one({"_id": ObjectId("111111111111111111111111")}, {"$set": {"em_status": True}})
+        db.electricity_meter.update_one({"_id": ObjectId("111111111111111111111111")}, {"$set": {"em_status": True}})
 
         # Add user information
         add_user_information()
 
     # # POST Requests
     # # Add Contract Route Success -> Doesnt work because mpo is not mocked
-    # def test_add_contract_route(self):
-    #     request_data = {
-    #         "electricity_meter_id": "111111111111111111111111",
-    #         "notes": "Test contract",
-    #         "address_plz": "12345",
-    #         "address_street": "Teststreet",
-    #         "address_street_house_number": "1",
-    #         "address_city": "Testcity",
-    #         "address_country": "Testcountry",
-    #     }
+    def test_add_contract_route_em_invalid(self):
+        pass
 
-    #     response = self.client.post('/add-contract', data=request_data)
-    #     assert response.status_code == 302
-    #     assert response.headers['Location'] == '/dashboard'
+'''
+    def test_add_contract_route(self):
+         request_data = {
+             "electricity_meter_id": "111111111111111111111111",
+             "address_plz": "12345",
+             "address_street": "Teststreet",
+             "address_street_number": "1",
+             "address_city": "Testcity",
+             "address_country": "Testcountry",
+             "notes": "Test contract"
+         }
 
-    #     # load all contracts of user
-    #     self.user = User.find_by_id(db=db, user_id=db.users.find_one({"username": "pytest"})["_id"]) # Get newest user object for updated contract list
-    #     contract_list = self.user['contract_list']
+         response = self.client.post('/add-contract', data=request_data)
+         assert response.status_code == 302
+         assert response.headers['Location'] == '/dashboard'
+
+         # load all contracts of user
+         self.user = User.find_by_id(db=db, user_id=db.users.find_one({"username": "pytest"})["_id"]) # Get newest user object for updated contract list
+         contract_list = self.user['contract_list']
         
-    #     logger.debug(f"Contract list: {contract_list}") #TODO Remove
+         logger.debug(f"Contract list: {contract_list}") #TODO Remove
 
-    #     assert len(contract_list) == 1
+         assert len(contract_list) == 1
 
-    #     # Check if contract is in contract list, else assert False
-    #     for contract_id in contract_list:
-    #         Contract = Contract_class.find_by_id(db, contract_id=contract_id)
-    #         if Contract != None and Contract["electricity_meter_id"] == request_data["electricity_meter_id"]:
-    #             assert True
-    #             return
+         # Check if contract is in contract list, else assert False
+         for contract_id in contract_list:
+             Contract = Contract_class.find_by_id(db, contract_id=contract_id)
+             if Contract != None and Contract["electricity_meter_id"] == request_data["electricity_meter_id"]:
+                 assert True
+                 return
         
-    #     assert False
+         assert False
 
-    # # Add Contract Route Fail
-    # def test_add_contract_route_em_already_exists(self):
-    #     request_data = {
-    #         "electricity_meter_id": "111111111111111111111111",
-    #         "notes": "Test contract",
-    #         "address_plz": "12345",
-    #         "address_street": "Teststreet",
-    #         "address_street_house_number": "1",
-    #         "address_city": "Testcity",
-    #         "address_country": "Testcountry"
-    #     }
+     # Add Contract Route Fail
+    def test_add_contract_route_em_already_exists(self):
+        request_data = {
+            "electricity_meter_id": "111111111111111111111111",
+             "address_plz": "12345",
+             "address_street": "Teststreet",
+             "address_street_number": "1",
+             "address_city": "Testcity",
+             "address_country": "Testcountry",
+             "notes": "Test contract"
+        }
 
-    #     # Add contract to user
-    #     self.test_add_contract_route()
+         # Add contract to user
+        self.test_add_contract_route()
 
-    #     response = self.client.post('/add-contract', data=request_data)
+        response = self.client.post('/add-contract', data=request_data)
 
-    #     assert response.status_code == 302
-    #     assert response.headers['Location'] == '/dashboard'
+        assert response.status_code == 302
+        assert response.headers['Location'] == '/dashboard'
 
-    #     # load all contract objects of user
-    #     self.user = User.find_by_id(db=db, user_id=db.users.find_one({"username": "pytest"})["_id"]) # Get newest user object for updated contract list
-    #     contract_list = self.user['contract_list']
+         # load all contract objects of user
+        self.user = User.find_by_id(db=db, user_id=db.users.find_one({"username": "pytest"})["_id"]) # Get newest user object for updated contract list
+        contract_list = self.user['contract_list']
         
-    #     logger.debug(f"Contract list: {contract_list}")
+        logger.debug(f"Contract list: {contract_list}")
 
-    #     assert len(contract_list) == 1
-
-    # def test_add_contract_route_em_invalid(self):
-    #     pass
+        assert len(contract_list) == 1
+'''
+    
